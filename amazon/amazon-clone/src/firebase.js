83436ecef62827eafbb9bdb5ce706d2753924f2e
Dibyam <dibyam1101@gmail.com>
2022-06-17 00:14:06 +0530

    Added the Header

---
+++ amazon/amazon-clone/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7c32cwQPqAH3MFd4KrEK7s-DzKHDnsgs",
+  authDomain: "clone-86d32.firebaseapp.com",
+  projectId: "clone-86d32",
+  storageBucket: "clone-86d32.appspot.com",
+  messagingSenderId: "16609895425",
+  appId: "1:16609895425:web:0ad762096758fd59760c00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
