6299518455882e6d284385d9cfd552b9f3ddf3f0
Dibyam <dibyam1101@gmail.com>
2022-06-20 21:00:09 +0530

    Added functionality to increment and decrement

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import "./App.css"
+import React from 'react'
+import "./App.css";
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import {incNumber, decNumber} from "./redux/actions/index"
 const App = () => {
+
+  // it alternative to the useContext hooks in react / consumer from context API 
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className='App'>
-      <h1>Welcome To Redux</h1>
-    </div>
-  );
+    <>
+      <div className="main-div">
+
+
+        <div className="container">
+
+          <h1>Increment/Decrement counter</h1>
+          <h4>using React and Redux</h4>
+
+          <div className="quantity">
+            <a className="quantity__minus" title="Decrement" onClick={() => dispatch(decNumber()) }><span>-</span></a>
+            <input name="quantity" type="text" className="quantity__input" value={myState} />
+            <a className="quantity__plus" title="Increment" onClick={() => dispatch(incNumber())}><span>+</span></a>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ redux/src/redux/actions/index.js
@@ -0,0 +1,8 @@
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+
+
+
+
+export const incNumber = () => ({ type: INCREMENT })
+export const decNumber = () => ({ type: DECREMENT })  
