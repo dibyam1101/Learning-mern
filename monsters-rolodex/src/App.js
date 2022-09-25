e60cdcb7d18bb55147a5aa2ea007c98c130c6160
Dibyam <dibyam1101@gmail.com>
2022-09-25 16:14:49 +0530

    Added API call

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monster: [
+
+      ],
+
+    };
+  }
+
+  async componentDidMount() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monster: users }
+          },
+          () => {
+            console.log(this.state)
+          }))
+
+  }
+
+  render() {
+    return <div className="App">
+
+
+      {/* {
+        this.state.monster.map((monster) => {
+          return <h1 key={monster.id}>{monster}</h1>
+        })
+      } */}
+    </div>;
+  }
 }
 
 export default App;
